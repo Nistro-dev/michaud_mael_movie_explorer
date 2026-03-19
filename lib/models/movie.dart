b31df115280f7ad50b1c5639569ca3ee4c7a58ae
Nistro-dev <micmacel86@gmail.com>
2026-03-19 15:09:54 +0100

    Add Movie model class

---
+++ lib/models/movie.dart
@@ -0,0 +1,21 @@
+class Movie {
+  final String title;
+  final String year;
+  final String poster;
+  final String plot;
+  final String director;
+  final String actors;
+  final String genre;
+  final String rating;
+
+  Movie({
+    required this.title,
+    required this.year,
+    required this.poster,
+    required this.plot,
+    required this.director,
+    required this.actors,
+    required this.genre,
+    required this.rating,
+  });
+}
