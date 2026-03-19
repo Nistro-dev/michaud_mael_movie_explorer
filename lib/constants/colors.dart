c1c79003ad978498c4525e4ab0e88866311c1956
Nistro-dev <micmacel86@gmail.com>
2026-03-19 15:19:43 +0100

    Add AppColors class for colors constants

---
+++ lib/constants/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const background = Color(0xFF1E1E1E);
+  static const cardBackground = Color(0xFF2D2D2D);
+  static const border = Color(0xFF404040);
+  static const textPrimary = Colors.white;
+  static const textSecondary = Colors.white60;
+  static const textTertiary = Colors.white70;
+  static const textDisabled = Colors.white38;
+  static const accent = Colors.amber;
+}
